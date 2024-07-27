1d2cfd79c9b129c7db5443b0a05bdf65ee1900ff
Pranay <Pranay@DESKTOP-1BP5002>
2024-07-27 18:48:25 +0530

    preincrement and post increment operators

---
+++ ConceptAndCoding/src/unaryOperators/PreAndPostIncrement.java
@@ -0,0 +1,12 @@
+package unaryOperators;
+
+public class PreAndPostIncrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 7;
+		 boolean res = a++ == 7 && ++a == 9 || a++ == 9 ;
+		 System.out.println(a + " " + res);
+	}
+
+}
