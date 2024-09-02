76765503ee91f8968eb1b787ce6a7ee3b4747de6
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-09-02 17:58:56 +0530

    PrefixString and suffix String

---
+++ ConceptAndCoding/src/strings/PrefixStringandSuffixString.java
@@ -0,0 +1,30 @@
+package strings;
+
+import java.util.Scanner;
+
+public class PrefixStringandSuffixString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String inputString = sc.next();
+		PSStringsMethod(inputString);
+	}
+	public static void PSStringsMethod(String stringName) {
+		
+		// prefix String -- all the SubString that start at 0th index and can end at any index
+		// suffix String -- all the SubString that end at last index and can begin at any index
+		for(int i=0; i<stringName.length()+1; i++) {
+			String substring = stringName.substring(0, i);
+			System.out.println(substring);
+		}
+		
+		for(int j = 0; j<=stringName.length();j++) {
+			String substring = stringName.substring(j, stringName.length());
+			System.out.println(substring);
+		}
+		
+	}
+
+}
