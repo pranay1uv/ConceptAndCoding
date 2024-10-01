919be45573158de0dc7c355e445c369eca0829ce
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-10-01 19:29:23 +0530

    Taking 2d array as input from scanner and Printing Elements

---
+++ ConceptAndCoding/src/MultiDimensionalArrays/PrintTwoDimArray.java
@@ -0,0 +1,38 @@
+package MultiDimensionalArrays;
+
+import java.util.Scanner;
+
+public class PrintTwoDimArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		// I have to receive 2d array fromm the scanner
+		
+		
+		
+		System.out.println("enter the no of rows");
+		int r = sc.nextInt();
+		System.out.println("enter the no of cols");
+		int c = sc.nextInt();
+		
+		int[][] twoDim = new int[r][c];
+		
+		for(int i = 0; i<r; i++) {
+			for(int j = 0; j<c; j++) {
+				twoDim[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int k = 0; k<twoDim.length; k++) {
+			for(int l = 0; l<twoDim[k].length; l++) {
+				System.out.print(twoDim[k][l]+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
